8436bfcc3d17c841197d67817fa0f829b9916c9c
Rahul Ilsa Tajri Mukhti <cengrahul00@gmail.com>
2026-05-05 15:23:04 +0700

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Menu;
 use App\Models\User;
+use App\Models\UserLevel;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +18,52 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // User::factory(10)->create();        
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        UserLevel::create([
+            'name' => 'Programmer',
+        ]);
+
+        UserLevel::create([
+            'name' => 'Staff Perpustakaan',
+        ]);
+
+        User::create([
+            'name' => 'Rahul Ilsa Tajri Mukhti',
+            'email' => 'cengrahul00@gmail.com',
+            'password' => Hash::make('PASS1234'),
+            'user_level_id' => 1,
+        ]);
+
+        User::create([
+            'name' => 'Yasmi Dyanti Maghfira',
+            'email' => 'yasmidyanti@gmail.com',
+            'password' => Hash::make('PASS1234'),
+            'user_level_id' => 2,
+        ]);
+
+        Menu::create([
+            'name' => 'User',
+            'route' => 'users.index',
+            'icon' => 'fas fa-fw fa-user',
+            'parent_id' => null,
+            'order' => '1',
+        ]);
+
+        Menu::create([
+            'name' => 'User Level',
+            'route' => 'user-levels.index',
+            'icon' => 'fas fa-fw fa-user-tag',
+            'parent_id' => null,
+            'order' => '2',
+        ]);
+
+        Menu::create([
+            'name' => 'Menu',
+            'route' => 'menus.index',
+            'icon' => 'fas fa-fw fa-list',
+            'parent_id' => null,
+            'order' => '3',
         ]);
     }
 }
